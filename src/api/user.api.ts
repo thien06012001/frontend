5c041d76b0b9af33a08e93c15dcb90a2f00e6b28
thien06012001 <thien06012001@gmail.com>
2025-04-07 16:53:02 +0700

    format code and apply endpoint

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,11 +1,12 @@
+import { api_endpoints } from '../constants/urls';
 import { http } from '../handlers/http.handler';
 import { User } from '../hooks/redux/slices/user.slice';
 
-export const getUserById = (id: string) => {
-  http.get<{ data: User }>(`/users/${id}`);
-};
+export const getUserById = (id: string) =>
+  http.get<{ data: User }>(api_endpoints.user.getById(id));
+
 export const updateUser = (id: string, data: Partial<User>) =>
-  http.put<{ data: User }>(`/users/${id}`, data);
+  http.put<{ data: User }>(api_endpoints.user.update(id), data);
 
 export const deleteUser = (id: string) =>
-  http.delete<{ data: boolean }>(`/users/${id}`);
+  http.delete<{ data: boolean }>(api_endpoints.user.delete(id));
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,5 +1,8 @@
 export const api_endpoints = {
   user: {
     ORIGIN: '/users',
+    getById: (id: string) => `/users/${id}`,
+    update: (id: string) => `/users/${id}`,
+    delete: (id: string) => `/users/${id}`,
   },
 };
